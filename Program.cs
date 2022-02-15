41d0242fa6fe4cb9f39f9dcf80dcb77dec0c3b45
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-02-16 00:06:53 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,32 @@
-﻿namespace ConsoleApp2
+﻿//7 - create a integer array of  3 elements and display the elements using for loop
+ // and foreach loop
+  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // wap to print no from 0 to 010 using while loop
-            int i, j;
-            for (i = 10; i <= 10; i++)
-            {
-                for (j = 10; j < i; j++)
-                {
-                    Console.Write("0" + "\t");
-                }
-                Console.WriteLine();
 
+            int[] arr = new int[3]; //creating an array of 3 elements
+            arr[0] = 11;
+            arr[1] = 22;
+            arr[2] = 33;
+            //using for loop 
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("arr = " + arr[i]);
+            }
+            //for each loop
+            //syntax foreach (datatype variable in arrayname) { //code }
+            foreach (int i in arr)
+            {
+                Console.WriteLine("arr = " + i);
             }
             Console.ReadLine();
 
