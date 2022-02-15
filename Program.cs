44b8ce8ef3bb350f0313f80dcbd34a3a275ea165
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-02-15 23:16:30 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,32 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace ConsoleApp3
+namespace ConsoleApp4
 {
     class Program
     {
         static void Main(String[] args)
         {
-            //wap to accept 2no. and display addition of two no.//
-            int num1, num2, res;
+            //wap to accept 3 no. and display addition of 3 no.//
+            int num1, num2, num3, res;
             Console.WriteLine("enter number1");
-
-
             num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("enter number2");
-
-
             num2 = Convert.ToInt32(Console.ReadLine());
-            res = num1 + num2;
-            Console.WriteLine("addition="+res);
+            Console.WriteLine("enter number3");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            res = num1 + num2 + num3;
+            Console.WriteLine("addition=" + res);
             Console.ReadLine();
-
-
-
-
-
-
-
         }
     }
 }
+
+
+
+
+
+
