0cfbdabcf139f5848259236316801848c3d153a1
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-02-15 23:12:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace 11-02-2020
+    {
+    class class1
+{
+    enum grade { dist = 90, first = 60, second = 40 }
+    public static void Main()
+    {
+        //wap to accept character from the user
+        grade gr;
+        gr = grade.dist;
+        Console.WriteLine("grade=" + gr);
+        Console.WriteLine("grade=" + gr);
+        Console.WriteLine("grade value=" + Convert.ToInt32(gr));
+
+
+    }
+}
+}
