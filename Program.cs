2dc52f7ffdda107dcc8f664c6d7d73e808b5e47e
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-02-16 00:01:26 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // wap to print no from 1 to 20 using while loop
-            int i;
-            for (i = 1; i <= 20; i++)
+            // wap to print no from 2 to 20 using while loop
+            int i, j;
+            for (i = 2; i < 10; i++)
             {
-                Console.Write("i = " + i);
+                for (j = 2; j <= 20; j++)
+                {
+                    Console.Write(j + "\t");
+                }
+                Console.WriteLine();
 
             }
             Console.ReadLine();
