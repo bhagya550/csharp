e7662a7d8a54dc3968ff75d243fbca65c19c5d12
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-02-16 10:59:14 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
 ﻿using System;
-public class PalindromeExample
+public class ArmstrongExample
 {
     public static void Main(string[] args)
     {
         int n, r, sum = 0, temp;
-        Console.Write("Enter the Number: ");
+        Console.Write("Enter the Number= ");
         n = int.Parse(Console.ReadLine());
         temp = n;
         while (n > 0)
         {
             r = n % 10;
-            sum = (sum * 10) + r;
+            sum = sum + (r * r * r);
             n = n / 10;
         }
         if (temp == sum)
-            Console.Write("Number is Palindrome.");
+            Console.Write("Armstrong Number.");
         else
-            Console.Write("Number is not Palindrome");
+            Console.Write("Not Armstrong Number.");
     }
 }
