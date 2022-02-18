a1d1126175eb3b3c16c245e424ad8bc3729e5955
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-02-18 12:47:03 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,33 +1,43 @@
-﻿//create a class employee having fiels empno,empname,and salary//
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace ConsoleApp
+namespace ConsoleApp2
 {
-
     class Employee
     {
-        public int empno;
-        public string empname;
-        public float salary;
+        int empno;
+        string empname;
+        float salary;
+        //creating methods
+        public void getdata()
+        {
+            Console.WriteLine("enter employee no");
+            empno = Convert.ToInt32(Console.ReadLine());
+            empname = Console.ReadLine();
+            Console.WriteLine("enter emplyee name");
+            empname = Console.ReadLine();
+            Console.WriteLine("enter employee salary");
+            salary = Convert.ToInt32(Console.ReadLine());
+        }
+        public void displaydata()
+        {
+            Console.WriteLine("empno" + empno);
+            Console.WriteLine("empname" + empname);
+            Console.WriteLine("salary" + salary);
+
+        }
 
     }
     class program
     {
         static void Main()
         {
-            //creating an ob]ect (instance) of employee class
-            //syntax classname objectname=new classname();
-            Employee emp = new Employee(1);
-            emp.empname = "sahil";
-            emp.salary = 12345.2f;
-            Console.WriteLine("empno" + emp.empno);
-            Console.WriteLine("empname" + emp.empname);
-            Console.WriteLine("salary" + emp.salary);
+            Employee emp = new Employee();
+            emp.getdata();
             Console.ReadLine();
-        }
 
+        }
     }
 }
