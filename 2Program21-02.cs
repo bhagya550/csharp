f6feeed79a2900235c29eea8cbfc10d2d9a7f1b6
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-02-28 13:13:44 +0530

    Add files via upload

---
+++ 2Program21-02.cs
@@ -0,0 +1,26 @@
+﻿//#protected access specifier//
+using System;
+namespace AccessSpecifiers
+{
+    class ProtectedTest
+    {
+        protected string name = "Shashikant";
+        protected void Msg(string msg)
+        {
+            Console.WriteLine("Hello " + msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProtectedTest protectedTest = new ProtectedTest();
+            // Accessing protected variable  
+            Console.WriteLine("Hello " + protectedTest.name);
+            // Accessing protected function  
+            protectedTest.Msg("Swami Ayyer");
+        }
+    }
+}
+
+
