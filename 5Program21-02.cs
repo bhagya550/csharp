9dda80963c9d41e6624ece606e6c514827ed47cb
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-02-28 13:18:23 +0530

    Add files via upload

---
+++ 5Program21-02.cs
@@ -0,0 +1,24 @@
+﻿//protected internal Access specifier//
+using System;
+namespace AccessSpecifiers
+{
+    class InternalTest
+    {
+        protected internal string name = "Shantosh Kumar";
+        protected internal void Msg(string msg)
+        {
+            Console.WriteLine("Hello " + msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InternalTest internalTest = new InternalTest();
+            // Accessing protected internal variable  
+            Console.WriteLine("Hello " + internalTest.name);
+            // Accessing protected internal function  
+            internalTest.Msg("Peter Decosta");
+        }
+    }
+}
