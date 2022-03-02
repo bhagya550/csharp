8c5f5c4b433fd18950b87caa0c2b46efa7fde8a1
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-02 21:00:53 +0530

    Add files via upload

---
+++ 2Program28-02.cs
@@ -0,0 +1,30 @@
+﻿//example of arrayth method//
+using System;
+using System.Collections;
+namespace ConsoleApp6
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add(11);
+            ar.Add(12);
+            ar.Add(13);
+            //displaying values of arraylist
+            foreach (int obj in ar)
+            {
+                Console.WriteLine("student rno " + obj);
+            }
+
+
+            Console.WriteLine("Capacity: {0} ", ar.Capacity);
+            Console.WriteLine("Count: {0}", ar.Count);
+
+
+
+        }
+    }
+}
