0f4eb44f63b87fff3046b089ba70930f5e67ed09
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-02 21:27:30 +0530

    Add files via upload

---
+++ 7Program28-02.cs
@@ -0,0 +1,15 @@
+﻿//simple example of namespace which contains one class "Program".//
+//namespace example//
+
+    using System;  
+    namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Namespace!");
+        }
+    }
+}
+
