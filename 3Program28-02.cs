89bd8e87660d32d2ddc226b0bcd8a9073ab25499
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-02 21:01:38 +0530

    Add files via upload

---
+++ 3Program28-02.cs
@@ -0,0 +1,36 @@
+﻿//example of sort method//
+using System;
+using System.Collections;
+namespace ConsoleApp6
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add(19);
+            ar.Add(12);
+            ar.Add(13);
+            //displaying values of arraylist
+            Console.WriteLine("array list colection without sorting");
+            foreach (int obj in ar)
+            {
+                Console.WriteLine("student rno " + obj);
+            }
+
+            //sorting
+            ar.Sort();
+            Console.WriteLine("array list colection with sorting");
+            foreach (int obj in ar)
+            {
+                Console.WriteLine("student rno " + obj);
+            }
+
+
+
+
+        }
+    }
+}
