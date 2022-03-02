7443271b41ec3cbe43bfe8c39be4de04fb7f78b2
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-02 21:00:08 +0530

    Add files via upload

---
+++ 1Program28-02.cs
@@ -0,0 +1,29 @@
+﻿//Example of array list//
+using System;
+using System.Collections;
+namespace ConsoleApp6
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add("amit");
+            ar.Add("raj");
+            ar.Add("dilip");
+            //displaying values of arraylist
+            foreach (Object obj in ar)
+            {
+                Console.WriteLine("student name " + obj);
+            }
+
+
+
+
+
+        }
+    }
+}
+
