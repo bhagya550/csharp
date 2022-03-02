204b613c084632d9ab8d660bdf02d5d2c012e837
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-02 20:01:30 +0530

    Add files via upload

---
+++ 3Program23-02.cs
@@ -0,0 +1,21 @@
+﻿//Drawing circle//
+//runtime polymorphism//
+using System;
+public class Animal
+{
+    public string color = "white";
+
+}
+public class Dog : Animal
+{
+    public string color = "black";
+}
+public class TestSealed
+{
+    public static void Main()
+    {
+        Animal d = new Dog();
+        Console.WriteLine(d.color);
+
+    }
+}
