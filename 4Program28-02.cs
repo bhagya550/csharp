89272f6b29edb4ced3977b492c322e5b3d168abf
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-02 21:02:29 +0530

    Add files via upload

---
+++ 4Program28-02.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+namespace ConsoleApp6
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add(19);
+            ar.Add(12);
+            ar.Add(13);
+            //displaying values of arraylist
+            Console.WriteLine("array list colection without sorting");
+            foreach (int obj in ar)
+            {
+                Console.WriteLine("student rno " + obj);
+            }
+            ar.Insert(2, 17);
+
+            Console.WriteLine("array list colection after adding element at 2 position");
+            foreach (int obj in ar)
+            {
+                Console.WriteLine("student rno " + obj);
+            }
+
+
+
+
+        }
+    }
+}
