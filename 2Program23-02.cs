982c100931094694e082cd216b09f34ba6c71be3
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-02 20:00:20 +0530

    Add files via upload

---
+++ 2Program23-02.cs
@@ -0,0 +1,43 @@
+﻿//C# Runtime Polymorphism Example//
+
+//Let's see a another example of runtime polymorphism in C# where we are having two derived classes.//
+
+    using System;  
+    public class Shape
+{
+    public virtual void draw()
+    {
+        Console.WriteLine("drawing...");
+    }
+}
+public class Rectangle : Shape
+{
+    public override void draw()
+    {
+        Console.WriteLine("drawing rectangle...");
+    }
+
+}
+public class Circle : Shape
+{
+    public override void draw()
+    {
+        Console.WriteLine("drawing circle...");
+    }
+
+}
+public class TestPolymorphism
+{
+    public static void Main()
+    {
+        Shape s;
+        s = new Shape();
+        s.draw();
+        s = new Rectangle();
+        s.draw();
+        s = new Circle();
+        s.draw();
+
+    }
+}
+
