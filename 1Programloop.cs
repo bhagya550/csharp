29c4952b1e09cbfe0bd8228ebc2ce9a787bf5fbd
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-03 10:53:53 +0530

    Add files via upload

---
+++ 1Programloop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class csharpExercise
+{
+    static void Main(string[] args)
+    {
+
+        char ch;
+
+        Console.WriteLine("Alphabets from a - z are: ");
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+            //Printing all alphabets with tab
+            Console.Write(ch + "\t");
+        }
+
+        Console.ReadLine();
+    }
+}
