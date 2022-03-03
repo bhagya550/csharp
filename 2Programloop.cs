9f035b1da0054dcad311aa82f0c87bfb02023661
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-03 10:55:09 +0530

    Add files via upload

---
+++ 2Programloop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class csharpExercise
+{
+    static void Main(string[] args)
+    {
+
+        int i;
+
+        // Printing ASCII values from 0 to 255
+        for (i = 0; i <= 255; i++)
+        {
+            Console.WriteLine("ASCII value of " + i + " = " + i);
+        }
+        Console.ReadLine();
+    }
+}
