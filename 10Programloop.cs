4c1e07b37cfa220dd11f1055b8d737e038d5d83b
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-09 22:24:26 +0530

    Add files via upload

---
+++ 10Programloop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class csharpExercise
+{
+    static void Main(string[] args)
+    {
+
+        int num, last;
+
+        // Reading number
+        Console.Write("Enter any number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        last = num % 10;
+        Console.WriteLine("The last digit of entered number: " + last);
+
+        while (num >= 10)
+        {
+            num = num / 10;
+        }
+
+        Console.WriteLine("The first digit of entered number: " + num);
+
+        Console.ReadLine();
+    }
+}
