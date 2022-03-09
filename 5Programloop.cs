a6c40264eed5a6b4b89e74682ba3596aa253f3a1
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-09 22:16:38 +0530

    Add files via upload

---
+++ 5Programloop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class csharpExercise
+{
+    static void Main(string[] args)
+    {
+
+        int num, total;
+
+        //Reading number
+        Console.WriteLine("Enter any number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        //Adding sum of digit in total variable
+        for (total = 0; num > 0; num = num / 10)
+            total = total + (num % 10);
+
+        //Printing sum of digit
+        Console.WriteLine("Sum of digits: " + total);
+
+        Console.ReadLine();
+    }
+}
