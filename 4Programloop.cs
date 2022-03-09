338f0b8d92430db0bd674ecca9721a6a5bf14b30
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-09 22:15:18 +0530

    Add files via upload

---
+++ 4Programloop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class csharpExercise
+{
+    static void Main(string[] args)
+    {
+
+        int i, num;
+
+        //Read a number from user
+        Console.Write("Enter any number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        /*Running loop from the number entered by user,
+          and Decrementing by 1*/
+        for (i = num; i >= 1; i--)
+        {
+            Console.WriteLine("\n" + i);
+        }
+
+        Console.ReadLine();
+    }
+}
