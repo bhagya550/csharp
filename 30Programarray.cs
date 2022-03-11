17505b9d5678491bda93db9457931abf9f59d3b4
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-11 10:42:49 +0530

    Add files via upload

---
+++ 30Programarray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+        int[] arrayA = new int[10];
+        int lengthA = arrayA.Length;
+        Console.WriteLine("Length of ArrayA : {0}", +lengthA);
+        long longLength = arrayA.LongLength;
+        Console.WriteLine("Length of the Long Length Array  : {0}", longLength);
+        int[,] twoD = new int[20, 50];
+        Console.WriteLine("The Size of 2D Array is : {0}", twoD.Length);
+        Console.ReadLine();
+    }
+}
