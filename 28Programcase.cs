7f38f80988335eb0126534c2049cc9b8c5f4f230
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-11 10:41:02 +0530

    Add files via upload

---
+++ 28Programcase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        char gender;
+
+        //Reading gender from user
+        Console.WriteLine("Enter gender (M/m or F/f): ");
+        gender = Convert.ToChar(Console.ReadLine());
+
+
+        // checking vowel and consonant
+        switch (gender)
+        {
+            case 'M':
+            case 'm':
+                Console.WriteLine("MALE");
+                break;
+
+            case 'F':
+            case 'f':
+                Console.WriteLine("FEMALE");
+                break;
+
+            default:
+                Console.WriteLine("Unspecified Gender");
+                break;
+        }
+
+        Console.ReadLine();
+
+    }
+
+}
