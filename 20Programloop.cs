f8bf5d8d8aa7e10c95e400275ed462ddc626927b
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-11 10:30:42 +0530

    Add files via upload

---
+++ 20Programloop.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class csharpExercise
+{
+    static void Main(string[] args)
+    {
+        int n, num = 0;
+
+        // Reading a number from user
+        Console.WriteLine("Enter any number to print in words: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        while (n != 0)
+        {
+            num = (num * 10) + (n % 10);
+            n /= 10;
+        }
+
+        //print corresponding digit in words till num becomes 0
+        while (num != 0)
+        {
+            switch (num % 10)
+            {
+                case 0:
+                    Console.Write("zero ");
+                    break;
+                case 1:
+                    Console.Write("one ");
+                    break;
+                case 2:
+                    Console.Write("two ");
+                    break;
+                case 3:
+                    Console.Write("three ");
+                    break;
+                case 4:
+                    Console.Write("four ");
+                    break;
+                case 5:
+                    Console.Write("five ");
+                    break;
+                case 6:
+                    Console.Write("six ");
+                    break;
+                case 7:
+                    Console.Write("seven ");
+                    break;
+                case 8:
+                    Console.Write("eight ");
+                    break;
+                case 9:
+                    Console.Write("nine ");
+                    break;
+            }
+            num = num / 10;
+
+
+
+        }
+        Console.ReadLine();
+    }
+}
