4ea1137e5f47aace6b3241a46c2e119734b6b855
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-11 10:45:22 +0530

    Add files via upload

---
+++ 33ProgramArray.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        int[] Arr = { 20, -10, -30, 0, 15, 10, 30 };
+        Console.WriteLine("Maximum Element : " + Arr.Max());
+        Console.WriteLine("Minimum Element : " + Arr.Min());
+        Console.Read();
+    }
+}
