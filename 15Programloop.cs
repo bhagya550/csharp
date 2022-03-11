5903949d7d3366e718dd76944cf45a0694b29319
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-11 10:25:57 +0530

    Add files via upload

---
+++ 15Programloop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class csharpExercise
+{
+    static void Main(string[] args)
+    {
+        int num, sum = 0, i, r;
+
+        // Reading number
+        Console.Write("Please enter a number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        //Finding armstrong number or not
+        for (i = num; i > 0; i = i / 10)
+        {
+            r = i % 10;
+            sum = sum + r * r * r;
+        }
+
+        if (num == sum)
+        {
+            Console.WriteLine(num + " is an armstrong number.");
+        }
+        else
+        {
+            Console.WriteLine(num + " is not an armstrong number.");
+        }
+
+        Console.ReadLine();
+    }
+}
