c75a1a78b96f87c2f133f7f1ae6fa976ca1f23b3
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-11 10:33:40 +0530

    Add files via upload

---
+++ 24Programloop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class csharpExercise
+{
+    static void Main(string[] args)
+    {
+
+        int weeknumber;
+
+        //Reading week no from user
+        Console.WriteLine("Enter week number(1-7): ");
+        weeknumber = Convert.ToInt32(Console.ReadLine());
+
+        switch (weeknumber)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+            default:
+                Console.WriteLine("Invalid input! Please enter week no. between 1-7.");
+                break;
+
+        }
+        Console.ReadLine();
+
+    }
+}
