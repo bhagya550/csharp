e2a86c5bfe25f93edb86c3c6fa0fe21238df64b8
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-11 10:23:11 +0530

    Add files via upload

---
+++ 12Programloop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class csharpExercise
+{
+    static void Main(string[] args)
+    {
+
+        int num, rev = 0;
+
+        // Reading number
+        Console.Write("Enter any number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        //finding reverse number using while loop
+        while (num > 0)
+        {
+            rev = rev * 10;
+            rev = rev + num % 10;
+            num = num / 10;
+        }
+
+        Console.Write("Reversed number is = " + rev);
+
+        Console.ReadLine();
+    }
+}
