0e73526e605de87413ff99bc65a7623d175fd3b9
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-14 19:46:30 +0530

    Add files via upload

---
+++ 47ProgramArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+
+        int[] array = new int[100];
+        int size, i;
+
+
+        //Reads size of the array
+        Console.WriteLine("Enter size of the array: ");
+        size = Convert.ToInt32(Console.ReadLine());
+
+        //Reads elements in array
+        Console.WriteLine("Enter elements in array: ");
+        for (i = 0; i < size; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        //Print array in reversed order
+        Console.WriteLine("\nArray in reverse order: ");
+        for (i = size - 1; i >= 0; i--)
+        {
+            Console.Write("\t" + array[i]);
+        }
+
+        Console.ReadLine();
+    }
+
+}
+
