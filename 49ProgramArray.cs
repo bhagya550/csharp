e482490c83866f8702fd4ba1dc15fa8a01d1f666
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-14 19:49:35 +0530

    Add files via upload

---
+++ 49ProgramArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class functionexcercise
+{
+    public static void welcome(string name)
+    {
+        Console.WriteLine("Welcome " + name);
+    }
+    public static void HaveAnice()
+    {
+        Console.WriteLine("Have a nice day");
+    }
+    public static void Main(string[] args)
+    {
+        string str1;
+        Console.Write("Please Enter a name : ");
+        str1 = Console.ReadLine();
+        welcome(str1);
+        HaveAnice();
+
+        Console.ReadLine();
+    }
+}
