0e3e3f25fc7544aa160d3952ef7e5098cdd13a77
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-14 19:51:41 +0530

    Add files via upload

---
+++ 52Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class functionexcercise
+{
+    public static int Countspaces(string str)
+    {
+        int spcctr = 0;
+        string strone;
+        for (int i = 0; i < str.Length; i++)
+        {
+            strone = str.Substring(i, 1);
+            if (strone == " ")
+                spcctr++;
+        }
+        return spcctr;
+    }
+    public static void Main()
+    {
+        string strtwo;
+        Console.Write("Please Enter a string : ");
+        strtwo = Console.ReadLine();
+        Console.WriteLine("\"" + strtwo + "\"" + " contains {0} spaces", Countspaces(strtwo));
+        Console.ReadLine();
+    }
+}
