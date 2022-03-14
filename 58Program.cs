da0f1607f046409cba010e0573a007ca94c3adad
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-14 19:58:48 +0530

    Add files via upload

---
+++ 58Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class functionexcercise
+{
+    static void Main()
+    {
+        decimal fact;
+        Console.Write("Enter a number : ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        fact = Factorial(num);
+        Console.WriteLine("The factorial of number {0} is  {1}", num, fact);
+        Console.ReadLine();
+    }
+    static decimal Factorial(int n1)
+    {
+        // The bottom of the recursion
+        if (n1 == 0)
+        {
+            return 1;
+        }
+        // Recursive call: the method calls itself
+        else
+        {
+            return n1 * Factorial(n1 - 1);
+        }
+    }
+}
