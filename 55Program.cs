b846e06478f2ce2fb34c19eb16a0ff8608d54228
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-14 19:54:53 +0530

    Add files via upload

---
+++ 55Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class functionexcercise
+{
+    public static bool checkprime(int num)
+    {
+        for (int i = 2; i < num; i++)
+            if (num % i == 0)
+                return false;
+        return true;
+    }
+    public static void Main()
+    {
+        Console.Write("Enter a number : ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        if (checkprime(n))
+            Console.WriteLine(n + " is a prime number");
+        else
+            Console.WriteLine(n + " is not a prime number");
+
+        Console.ReadLine();
+    }
+}
