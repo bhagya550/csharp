c09827cd2ab6f03f13ee913abbd3bd386666375b
bhagya550 <95204183+bhagya550@users.noreply.github.com>
2022-03-14 19:56:01 +0530

    Add files via upload

---
+++ 56Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class functionexcercise
+{
+    public static int Fibonaccisequence(int number)
+    {
+        int num1 = 0;
+        int num2 = 1;
+
+        for (int i = 0; i < number; i++)
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp + num2;
+        }
+        return num1;
+    }
+    public static void Main()
+    {
+        Console.Write("Enter a number : ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("The Fibonacci series of " + num + " numbers is :");
+        for (int i = 0; i < num; i++)
+        {
+            Console.Write(Fibonaccisequence(i) + "  ");
+        }
+        Console.ReadLine();
+    }
+}
+
